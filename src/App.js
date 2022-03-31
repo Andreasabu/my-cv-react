a07d567d4a383177ba322b8714c1953a217005f5
Andrea <astamariab@gmail.com>
2022-03-31 19:01:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
+
+import {Hero, About, Education, Experience, More} from "./components";
+import {CV} from "./CV/CV";
+
+const {hero, education, experience, languages, skills} = CV;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Hero hero={hero} />
+        <About hero={hero} />
+        <Education education={education} />
+        <Experience experience={experience} />
+	      <More
+        languages={languages}
+        skills={skills}
+      
+	      />
     </div>
   );
 }
---
+++ src/CV/CV.js
@@ -0,0 +1,92 @@
+export const CV={
+    hero: {
+        name:"Andrea",
+        lastname: "Santa María",
+        city: "Barcelona",
+        email: "andrea@gmail.com",
+        birthDate: "2/04/1990",
+        phone: "(+12) 767333841",
+        image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e647605a-8e2c-4ed7-98ae-754a8d672b6a/dejmktq-5ae2f632-fa75-459f-bf20-480dacd2ccf3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2U2NDc2MDVhLThlMmMtNGVkNy05OGFlLTc1NGE4ZDY3MmI2YVwvZGVqbWt0cS01YWUyZjYzMi1mYTc1LTQ1OWYtYmYyMC00ODBkYWNkMmNjZjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LcunB9_ObausoAmNe1ikbJDrbsnHgizfflGxjZA_fUE",
+        gitHub: "https://github.com/andreasabu",
+        aboutMe: [
+            {
+              info:"I am an experienced international professional with a solid background in the private education industry.",
+            },
+            {
+              info: "🕶 Genius, future billionaire, playgirl, philanthropist.",
+            },
+            {
+              info: "YOLO is my motto",
+            },
+          ],
+        },
+          education: [
+            {
+              name: "Web Developer fullstack",
+              date: "2021/2022",
+              where: "UpgradeHub",
+            },
+            {
+              name: "Tourism and Leisure Management",
+              date: "2011",
+              where: "IMC",
+            },
+            {
+                name: "Grado en Turismo",
+                date: "2013",
+                where: "UAB",
+            },
+          ],
+          experience: [
+            {
+              name: "Administrative Specialist",
+              date: "2018 – Nowadays",
+              where: "EF Education First",
+              description:"Learning for the new world of work",
+               
+            },
+            {
+              name: "Senior Student Advisor",
+              date: "2016-2018",
+              where: "EF Education First",
+              description:
+                "We combine the expertise and agility of a boutique learning partner with the resources of a trusted, global organization.",
+            },
+          ],
+          languages: [{
+            language: "English",
+            wrlevel: "Advanced",
+            splevel: "Advanced",
+          },
+          {
+            language: "Spanish",
+            wrlevel: "Advanced",
+            splevel: "Advanced",
+          },
+          {
+            language: "German",
+            wrlevel: "Intermediate",
+            splevel: "Intermediate",
+          },
+          {
+            language: "Italian",
+            wrlevel: "pre Intermediate",
+            splevel: "pre Intermediate",
+          },
+          {
+            language: "French",
+            wrlevel: "pre Intermediate",
+            splevel: "pre Intermediate",
+          },
+        ],
+          skills: [
+            "Problem-solving mindset",
+            "Communication skills",
+            "Independance",
+            "Team Spirit",
+            "Money",
+            "Dating",
+            "Saving the world",
+          ],
+    
+}
